f62488be69f37e0f4e50557ff310f855c82659b9
Patrick Amstad <patrick.amstad@philico.com>
2025-12-04 20:16:18 +0100

    fix multiplier width of input field

--- value-investing-frontend/value-investing/src/pages/Analysis/Components/LiquidationValue.jsx
+++ value-investing-frontend/value-investing/src/pages/Analysis/Components/LiquidationValue.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 import { computeNetLiqValue } from "./selectorFunctions";
 import { getCurrencySymbol } from "../../valuation/components/selectorFunctions";
 
-const tableColumnWidths = ["57%", "20%", "3%", "15%"];
+const tableColumnWidths = ["50%", "20%", "10%", "15%"];
 
 function LiquidationValue({
   qfsSymbol,
